9834a10504a51d6f05d592f80d6a9c261763ba33
blossun <dnsngu@gmail.com>
2019-09-24 22:48:21 +0900

    PHP데이터 타입

---
+++ web2_php/number.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<body>
+  <h1>Number & Arithemtic Operator </h1>
+  <h2>1+1</h2>
+  <?php
+    echo 1+1; //2에 대한 표현식
+  ?>
+  <h2>2-1</h2>
+  <?php
+    echo 2-1; //1에 대한 표현식
+  ?>
+  <h2>2*2</h2>
+  <?php
+    echo 2*2; //4에 대한 표현식
+  ?>
+  <h2>4/2</h2>
+  <?php
+    echo 4/2; //2에 대한 표현식
+  ?>
+</body>
+</html>
---
+++ web2_php/string.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<body>
+<?php
+  echo strlen("Hello World");
+ ?>
+</body>
+</html>
+<!--
+echo 'Hello world <br/>';
+echo "Hello world <br/>";
+echo 'Hello "w"orld <br/>';
+echo "Hello 'w'orld <br/>";
+echo "Hello \"w\"orld <br/>"; //escape character
+
+echo "Hello"."world";
+
+-->

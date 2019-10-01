848bce4c8c1f4771033655d10954229107c4ad31
blossun <dnsngu@gmail.com>
2019-10-01 22:42:48 +0900

    PHP 보안 - xss, 파일 경로 보호

--- web2_php/lib/print.php
+++ web2_php/lib/print.php
@@ -1,14 +1,15 @@
 <?php
 function print_title(){
   if(isset($_GET['id'])){
-    echo $_GET['id'];
+    echo htmlspecialchars($_GET['id']);
   }else{
     echo "Welcome";
   }
 }
 function print_content(){
   if(isset($_GET['id'])){
-    echo file_get_contents('./data/'.$_GET['id']);
+    $basename = basename($_GET['id']);
+    echo htmlspecialchars(file_get_contents('./data/'.$basename));
   }else{
     echo "This is Javakong Homepage.";
   }
@@ -17,9 +18,10 @@ function file_list(){
   $list = scandir('./data/');
   $i=0;
   while($i<count($list)){
+    $title = htmlspecialchars($list[$i]);
     if($list[$i] != '.'){
       if($list[$i] != '..'){
-        echo "<li><h3><a href=\"index.php?id=$list[$i]\">$list[$i]</a></h3></li>\n";
+        echo "<li><h3><a href=\"index.php?id=$title\">$title</a></h3></li>\n";
       }
     }
     $i++;

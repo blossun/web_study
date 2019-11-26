aa42fecbaa1091d0ee1fb8a0cb9ffcd27b936059
blossun <dnsngu@gmail.com>
2019-11-27 01:51:39 +0900

    리팩토링 - 객체화
    game 클래스를 생성해서 객체의 속성과 메소드로 정의

--- js-playground/puzzle02.js
+++ js-playground/puzzle02.js
@@ -1,37 +1,17 @@
 
-var getStr = function () {
-    var str = document.getElementById('example').innerHTML.split('');
-    console.log(str);
-    return str;
-};
-var reversing = function (ans) {
-    // console.log(ans);
-    var str = ans.reverse();
-    btnprint(str);
-    console.log(str);
-    checkAns();
-};
-var pushing = function (ans) {
-    //오른쪽 밀어내기로 수정
-    // var left = ans.shift();
-    // ans.push(left);
-    var right = ans.pop();
-    ans.unshift(right);
-    btnprint(ans);
-    checkAns();
 
-}
-var btnprint = function (ans) {
-    // var str = '';
+var game = {
+    'quiz' : document.getElementById('example').innerHTML.split(''),
+    'ans' : document.getElementById('example').innerHTML.split('')
+};
+game.btnprint = function () {
     var contents = document.getElementById('contents');
-    for (var i = 0; i < ans.length; i++) {
-        // str += "<input type=\"button\" id=\"btn"+i+"\" value=\""+ans[i]+"\">";
-        // btn 생성을 createElement() 로 수정
+    for (var i = 0; i < this.ans.length; i++) {
         var btn = document.createElement('input');
         btn.type = 'button';
         btn.id = "btn" + i;
-        btn.value = ans[i];
-        if (contents.childElementCount != ans.length) {
+        btn.value = this.ans[i];
+        if (contents.childElementCount != this.ans.length) {
             contents.appendChild(btn);
         } else {
             contents.removeChild(contents.firstChild);
@@ -39,20 +19,32 @@ var btnprint = function (ans) {
         }
 
     }
-    // document.getElementById('contents').innerHTML=str;
 }
-function checkAns() {
-    if (quiz.toString() == ans.toString()) {
+game.checkAns = function() {
+    if (game.quiz.toString() == game.ans.toString()) {
         document.getElementById('result').innerHTML = "일치합니다.";
     } else {
         document.getElementById('result').innerHTML = "일치하지 않습니다.";
     }
 }
-function main() {
-    btnprint(quiz);
-    checkAns();
 
+game.reversing = function (event) {
+    var str = game.ans.reverse();
+    game.btnprint(str);
+    game.checkAns();
+};
+game.rpushing = function (event) { //오른쪽으로 밀기
+    var right = game.ans.pop();
+    game.ans.unshift(right);
+    game.btnprint();
+    game.checkAns();
 }
-quiz = getStr();
-ans = getStr();
-main();
+game.lpushing = function(event){ //왼쪽으로 밀기
+    var left = game.ans.shift();
+    game.ans.push(left);
+    game.btnprint();
+    game.checkAns();
+}
+
+
+game.btnprint(); //맨처음 문자를 버튼에 표시해주기

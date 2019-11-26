1622de821419258fb9e8520a9717e3e64b8056ce
blossun <dnsngu@gmail.com>
2019-11-26 18:40:17 +0900

    구구단프로그램 객체화 및 공백없이 입력받아 처리하도록 수정

--- js-playground/calc01.js
+++ js-playground/calc01.js
@@ -1,13 +1,15 @@
 console.log("calc01.js start");
-var calc = function(){
-    var formula = document.getElementById('inputStr').value.split(' ');
+
+calculator = {}
+calculator.calc = function(){
+    var formula = document.getElementById('inputStr').value.split("");
     console.log(formula);
     // 2 + 3 * 4 / 2 = 10
     var result = Number(formula[0]);
     var num;
     for(i=1; i<formula.length; i++){
         var value = formula[i]
-        if(value.match(/\+|\-|\*|\//) != null){
+        if(value.match(/\+|\-|\*|\//) != null){ //정규표현식 연산자인 경우 처리
             //연산기호인 경우
             console.log(value);
             switch(value){
@@ -23,8 +25,15 @@ var calc = function(){
             console.log('result : ' , result);
         }
     }
-    // document.write(result);
+    output.print(result);
+    // return result;
+
+};
+
+//리팩토링 - 객체화
+output = {}
+output.print = function(result){ //리팩토링 - 함수화
+    console.log("print start");
     document.getElementById('result').innerHTML = result;
     console.log('result : ',result);
-
-}
+};

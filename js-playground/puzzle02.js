7fbf2d3607d2363db9fed7c923ba9477699debfe
blossun <dnsngu@gmail.com>
2019-11-27 17:37:16 +0900

    버튼 기능 수정
    - random으로 문제 재 출제시, 이미 출력된 버튼의 갯수와 새로운 문자의 길이가 같은 경우 에러 발생
    - print 한 버튼 요소들을 먼저 다 remove 해주고, random을 재출제시 새로 append 해줌
    
    맞춘갯수 체크하는 score 객체 생성

--- js-playground/puzzle02.js
+++ js-playground/puzzle02.js
@@ -17,35 +17,46 @@ game.suffle = function(){ //버튼으로 배열된 단어위 위치를 무작위
     while(this.ans.length >0){
         // var t = this.ans.pop(this.ans[Math.floor(Math.random() * this.ans.length)]); //랜덤으로 뽑아서 pop 해서 새로운 배열에 넣어주기
         //pop은 무조건 맨우측 값을 빼니깐,,,, 이거 말고,,,
-        var i = Math.floor(Math.random() * this.ans.length);
-        var t = this.ans.splice(i, 1);
+        var i = Math.floor(Math.random() * this.ans.length); //단어길이만큼의 범위에서 random값을 뽑아서 index로 쓰기
+        var t = this.ans.splice(i, 1); //해당 index문자를 빼서 임시 temp 배열에 넣기
         this.temp.push(t);
         console.log(this.temp);
     }
     this.ans = Array.from(this.temp);
-    this.temp = [];
+    this.temp = []; //임시배열 다시 비워주기
     this.btnprint();
-
+}
+game.suffle2 = function(){ //단어를 뒤집거나 밀어내기 기능만으로 섞어주는 기능
+    var toggle = Math.floor(Math.random() * 2) === 0;
+    if(toggle){
+        this.reversing();
+    }
+    var n = Math.floor(Math.random() * this.ans.length) + 1;
+    for(var i=0; i<n; i++){
+        this.rpushing();
+    }
 }
 game.btnprint = function () {
     var contents = document.getElementById('contents');
+    while( contents.hasChildNodes()){ //기존에 print한 버튼 지우기
+        contents.removeChild(contents.firstChild);
+    }
     for (var i = 0; i < this.ans.length; i++) {
         var btn = document.createElement('input');
         btn.type = 'button';
         btn.id = "btn" + i;
         btn.value = this.ans[i];
-        if (contents.childElementCount != this.ans.length) {
-            contents.appendChild(btn);
-        } else {
-            contents.removeChild(contents.firstChild);
-            contents.appendChild(btn);
-        }
+        
+        contents.appendChild(btn);
 
     }
 }
 game.checkAns = function() {
     if (game.quiz.toString() == game.ans.toString()) {
         document.getElementById('result').innerHTML = "일치합니다.";
+        score.count += 1;
+        score.print();
+        game.startRound();
     } else {
         document.getElementById('result').innerHTML = "일치하지 않습니다.";
     }
@@ -68,6 +79,28 @@ game.lpushing = function(event){ //왼쪽으로 밀기
     game.btnprint();
     game.checkAns();
 }
+game.startRound = function(){
+    game.random(); //랜덤 퀴즈 문제 선택
+    game.suffle2();
+    game.btnprint(); //맨처음 문자를 버튼에 표시해주기
+}
 
-game.random(); //랜덤 퀴즈 문제 선택
-game.btnprint(); //맨처음 문자를 버튼에 표시해주기
+
+var score = {
+    'count' : 0
+}
+score.print = function(){
+    var result = document.getElementById('score');
+    var c = document.createElement('span');
+    c.innerHTML = 'O';
+    result.appendChild(c);
+    if(this.count == 3){
+        var c = document.createElement('span');
+        c.innerHTML = 'Thank you for playing!';
+        result.appendChild(c);
+    }
+};
+
+
+game.startRound(); //라운드시작
+score.count = 0; //초기점수 0 으로 셋팅
